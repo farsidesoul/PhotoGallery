5d33f445b1d0af13328ffb1e637805be75632d22
Darron Lumley <dlumley@iinet.net.au>
2015-01-27 21:22:11 +1100

    Initial commit

---
+++ app/src/main/java/farsidesoul/com/photogallery/GalleryItem.java
@@ -0,0 +1,38 @@
+package farsidesoul.com.photogallery;
+
+/**
+ * Created by ElephantPC on 27/01/2015.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String toString(){
+        return mCaption;
+    }
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
